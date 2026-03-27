e4bd890348073e83c0e1edaf69d6a262b5b957b9
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-03-27 07:30:11 -0400

    added warning msg for empty url; corrected posting_url label & placeholder

--- pages/help_pets.py
+++ pages/help_pets.py
@@ -9,15 +9,26 @@ st.header("Help Pets")
 st.space()
 
 posting_url = st.text_input(
-    label = "Webpage URL (e.g, https://www.lostmydoggie.com/details.cfm?petid=473750)",
-    placeholder = "Paste URL here",
+    label = "Webpage URL",
+    placeholder = "https://www.lostmydoggie.com/details.cfm?petid=473750",
     key = "poster_url"
 )
 
 def handle_urlscanbtn_click():
-    pass
+    url = st.session_state["poster_url"].strip()
 
-urlscanbtn = st.button(
-    label = "Scan URL",
-    on_click = handle_urlscanbtn_click
-)
+    st.session_state["url_warning"] = ""
+    if not url:
+        st.session_state["url_warning"] = 'Enter a URL first! Then, click "Scan URL"'
+        return
+
+url_btn_col, warning_col = st.columns([1, 6])
+with url_btn_col:
+    urlscanbtn = st.button(
+        label = "Scan URL",
+        type = "primary",
+        on_click = handle_urlscanbtn_click
+    )
+
+with warning_col:
+    st.error(st.session_state["url_warning"])
