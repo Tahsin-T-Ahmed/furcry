cfe71dafb0c050c9d26e26cd608d1f1b1d8ed613
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-03-27 05:47:22 -0400

    completed landing page

--- pages/home.py
+++ pages/home.py
@@ -5,3 +5,20 @@ st.set_page_config(
 )
 
 st.title("FurCry: Missing Pets Need You!")
+
+st.divider()
+
+st.header("What is FurCry?")
+st.caption("""
+    FurCry takes information from alerts of Lost/Found pets,
+    and processes it into a sharable format for spreading awareness 
+    on various platforms (such as social media, messaging, email, etc) 
+    as quickly and efficiently as possible.
+""")
+
+st.markdown("Ready to start helping? Go to the :red[Help Pets] tab or click below:")
+st.page_link(
+    page = "./pages/help_pets.py",
+    label = "Help Pets",
+    icon = ":material/pets:"
+)
