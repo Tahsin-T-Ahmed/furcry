2f18fe2ffd18cd2b0935da2d4d4658f40cedd210
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-03-27 06:19:28 -0400

    added title and URL scanner button (with handler function); assigned var names to elements

--- pages/help_pets.py
+++ pages/help_pets.py
@@ -1,6 +1,16 @@
 import streamlit as st
 
-st.text_input(
+st.header("Help Pets")
+st.space()
+posting_url = st.text_input(
     label = "Webpage URL (e.g, https://www.lostmydoggie.com/details.cfm?petid=473750)",
     placeholder = "Paste URL here"
+)
+
+def handle_urlscanbtn_click():
+    pass
+
+urlscanbtn = st.button(
+    label = "Scan URL",
+    on_click = handle_urlscanbtn_click
 )
