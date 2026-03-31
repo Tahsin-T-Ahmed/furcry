4b5fd917da438f7ce9548345f08f62d0a06165ea
Tahsin-T-Ahmed <tahsin.t.ahmed@gmail.com>
2026-03-31 11:57:20 -0400

    parsed title and date from Petco Love Lost

--- scraping/pll_scraper.py
+++ scraping/pll_scraper.py
@@ -12,8 +12,17 @@ def scrape(url:str) -> dict:
     dom = BeautifulSoup(response.text, "html.parser")
 
     posting_info = {}
+
+    header_words = dom.find("h1").text.split(' ')
+    posting_info["TITLE"] = ' '.join(header_words[:2])
     
-    posting_info["TITLE"] = ' '.join(dom.find("h1").text.split(' ')[:2])
+    lost_or_found = header_words[0]
+    date = dom.find(string=f"{lost_or_found} Date").next_element.text
+    date_terms = date.split('/')
+    date = f"{date_terms[2]}-{int(date_terms[0]):02d}-{int(date_terms[1]):02d}"
+    posting_info[f"DATE {lost_or_found.upper()}"] = date
+
+    
     
     return posting_info
 
